218a2406c07eb1d90ebd2d5c7a1066f75858f83b
Chen Huang <huangchen.buaa@gmail.com>
2020-03-17 21:17:24 -0700

    28.implement-str-str

--- 28.implement-str-str.py
+++ 28.implement-str-str.py
@@ -8,8 +8,20 @@
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle in haystack:
-            return haystack.index(needle)
+        if len(needle) == 0:
+            return 0
+        if len(haystack) < len(needle):
+            return -1
+        i = 0
+        while i <= len(haystack) - len(needle):
+            j = 0
+            while j < len(needle):
+                if haystack[i + j] != needle[j]:
+                    break
+                j += 1
+            if j == len(needle):
+                return i
+            i += 1
         return -1
 
 
