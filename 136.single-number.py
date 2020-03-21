849da72387be6e9747443ad442c5addaa62ca038
Chen Huang <huangchen.buaa@gmail.com>
2020-03-21 16:08:56 -0700

    136.single-number

--- 136.single-number.py
+++ 136.single-number.py
@@ -5,12 +5,25 @@
 #
 
 # @lc code=start
-class Solution:
+
+
+class Solution_Set:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+        for num in nums:
+            if num in s:
+                s.remove(num)
+            else:
+                s.add(num)
+        return s.pop()
+
+
+class Solution_Bit_Manipulation:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
         for num in nums:
             res ^= num
         return res
-        
-# @lc code=end
 
+
+# @lc code=end
