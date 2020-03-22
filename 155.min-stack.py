3f9e6f09d9c5e027327ae429c0fe87731f8d6596
Chen Huang <huangchen.buaa@gmail.com>
2020-03-21 23:25:37 -0700

    155.min-stack

---
+++ 155.min-stack.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=155 lang=python3
+#
+# [155] Min Stack
+#
+
+# @lc code=start
+import sys
+
+
+class MinStack:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self) -> None:
+        res = self.stack[-1]
+        self.stack.pop(-1)
+        if res == self.min_stack[-1]:
+            self.min_stack.pop(-1)
+        return res
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
