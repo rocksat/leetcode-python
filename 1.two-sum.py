bb77195440c85b559e4c45a430521c1b2576c199
Chen Huang <huangchen.buaa@gmail.com>
2020-03-23 19:16:37 -0700

    1.two-sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -4,9 +4,20 @@
 # [1] Two Sum
 #
 
-
 # @lc code=start
+
+
 class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lut = dict()
+        for idx, num in enumerate(nums):
+            if target - num in lut:
+                return [idx, lut[target - num]]
+            else:
+                lut[num] = idx
+
+
+class Solution_HashTable:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ht = dict()
         for idx, val in enumerate(nums):
